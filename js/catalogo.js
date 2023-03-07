caad666b31ff92847b680c0f8710ebb59846d331
Josué Acevedo <105468315+JosueAcevedo99@users.noreply.github.com>
2023-03-07 14:15:53 -0600

    version 1.2

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,16 +1,12 @@
 
 
+const dataBaseUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSjgXTjtZCnlVM2ESGxnUKh6Y1RJDndriL_Yz0vVm-EVrf-4FxkF30nmR9YQerO5_OIe6BkfKUh2zqN/pub?gid=0&single=true&output=csv';
 
 
+let dataBase = null;
 
-
-const cargarCatalogo = async () => {
-
-    const container = document.querySelector('#portafolio');
-    let contentHTML = '';
-
-  
-    await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vSjgXTjtZCnlVM2ESGxnUKh6Y1RJDndriL_Yz0vVm-EVrf-4FxkF30nmR9YQerO5_OIe6BkfKUh2zqN/pub?gid=0&single=true&output=csv')
+async function cargarDatos() {
+    await fetch(dataBaseUrl)
       .then(response => response.text())
       .then(data => {
         const lines = data.split('\n')
@@ -27,9 +23,26 @@ const cargarCatalogo = async () => {
         }
   
         const jsonData = JSON.stringify(result)
-        const data2 = JSON.parse(jsonData)
-        for (let i = 0; i < data2.length; i++) {
-          const obj = data2[i]
+        dataBase = JSON.parse(jsonData)
+        cargarCatalogo();
+
+        
+
+        
+      }).catch(error => console.error(error))
+}
+
+window.addEventListener('load', cargarDatos);
+
+
+const cargarCatalogo = async () => {
+
+
+    const container = document.querySelector('#portafolio');
+    let contentHTML = '';
+
+        for (let i = 0; i < dataBase.length; i++) {
+          const obj = dataBase[i]
           const image = obj.Img;
           const tittle = obj.Producto;
           const precio = obj.Precio;
@@ -43,11 +56,51 @@ const cargarCatalogo = async () => {
                       <a href="${vinculo}" target="_blank" class="btn btn-outline-light btnImprimirCatalogo" >Ver más</a>
                       </div>`;
         }
-      })
-      .catch(error => console.error(error))
     container.innerHTML = contentHTML;
-  }
+}
   
-  cargarCatalogo();
+async function categoria(categoria) {
+
+
+    const container = document.querySelector('#portafolio');
+    let contentHTML = '';
+    if(categoria == "todo"){
+      for (let i = 0; i < dataBase.length; i++) {
+          const obj = dataBase[i]
+          const image = obj.Img;
+          const tittle = obj.Producto;
+          const precio = obj.Precio;
+          const vinculo = obj.vinculo;
   
+          contentHTML += `
+                <div class="col-md-4 text-center mt-3 my-2">
+                      <img src="${image}" class="img-thumbnail imgCatalogo">
+                      <h5 class="title mt-3"> ${tittle}</h5>
+                      <h5 class="title mt-3">$ ${precio}</h5>
+                      <a href="${vinculo}" target="_blank" class="btn btn-outline-light btnImprimirCatalogo" >Ver más</a>
+                      </div>`;
+
+        
+      }
+    }
+
+        for (let i = 0; i < dataBase.length; i++) {
+          if(dataBase[i].categoria == categoria){
+            const obj = dataBase[i]
+            const image = obj.Img;
+            const tittle = obj.Producto;
+            const precio = obj.Precio;
+            const vinculo = obj.vinculo;
+    
+            contentHTML += `
+                  <div class="col-md-4 text-center mt-3 my-2">
+                        <img src="${image}" class="img-thumbnail imgCatalogo">
+                        <h5 class="title mt-3"> ${tittle}</h5>
+                        <h5 class="title mt-3">$ ${precio}</h5>
+                        <a href="${vinculo}" target="_blank" class="btn btn-outline-light btnImprimirCatalogo" >Ver más</a>
+                        </div>`;
   
+          }
+        }
+    container.innerHTML = contentHTML;
+}
